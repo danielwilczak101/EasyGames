2a5c7d58700ae1344f5021a1bc5b22eed5fe8bc4
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-07-16 14:55:16 -0500

    website updated with javascript working

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request
+import json
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -6,6 +7,17 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/')
 def index():
     return render_template('main.html')
+
+
+@app.route('/game_tree', methods=['GET', 'POST'])
+def parse_request():
+    # Grab the state of the map plus the move.
+    map = json.loads(request.form['map'])
+
+    # A.I changed move
+    map[7] = "2"
+    print(map)
+    return jsonify({"status": "Pc lost", "data": map})
 
 
 if __name__ == '__main__':
